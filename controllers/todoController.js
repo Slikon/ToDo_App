e3110e0cd205cd834646521e29fa83816fb73486
Slikon <slava.kulik27@gmail.com>
2020-06-25 16:32:04 +0300

    added: get/post/delete requests in MongoDB; deleted: hardcode elements from view, hardcoded element from MongoDB collection

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -13,38 +13,33 @@ let todoSchema = new mongoose.Schema({
 
 let Todo = mongoose.model('Todo', todoSchema)
 
-let todoOne = Todo({
-    item: 'buy milk'
-}).save((err) => {
-    if (err) throw err;
-    console.log('item saved')
-})
-
-let data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'Play wow'}]
 let urlencodedParser = bodyParser.urlencoded({ extended: false})
 
 module.exports = (app) => {
 
     app.get('/todo', (req, res) => {
-        res.render('todo', {todos: data})
+        //get data from mongoDB and pass it to the view
+        Todo.find({}, (err, data) => {
+            if(err) throw err;
+            res.render('todo', {todos: data})
+        })
     })
 
     app.post('/todo', urlencodedParser,  (req, res) => {
-        data.push(req.body)
-        console.log('ELEMENT ADDED CONTROL')
-        res.json(data)
+        //get data from view and pass it to MongoDB
+        let newTodo = Todo(req.body).save( (err, data) => {
+            if (err) throw err;
+            res.json(data)
+        })
     })
 
     app.delete('/todo/:item', (req, res) => {
-        
-        data = data.filter((todo) => {
-            
-            return todo.item.replace(/ /g, "-") !== req.params.item
+        console.log('delete detected')
+        //delete requested item from MongoDB
+        Todo.find({item: req.params.item}).deleteOne( (err, data) => {
+            if(err) throw err;
+            res.json(data)
         })
-        
-        console.log(data)
-        res.json({todos: data})
-        
     })
 
 }
